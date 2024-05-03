c0bc64c7d00b4c0542b2d69681970135e1c49960
ChristusKistus <93676725+ChristusKistus@users.noreply.github.com>
2024-05-03 17:04:35 +0200

    Update veronese_short.cpp

--- veronese_short.cpp
+++ veronese_short.cpp
@@ -31,9 +31,10 @@ bool all_before_index_zero(std::vector<short> input, short index) {
 
 
 int binomial(short n,short r) {
-	int num=fac(n);
-	int denom=fac(n-r)*fac(r);
-	return num/denom;
+	if (r==0) {
+		return 1;
+	}
+	return (binomial(n,r-1)*(n-r+1))/(r);
 }
 
 std::vector<short> add(std::vector<short> v1, std::vector<short> v2) {
@@ -55,8 +56,8 @@ std::vector<short> next_vector(std::vector<short>& input, short d) {
 	}
 	std::vector<short> output=input;
 	int index=find_index_to_change(input);
-	if (sum>d) {
-		throw std::invalid_argument("vector sum too big");
+	if (sum!=d) {
+		throw std::invalid_argument("vector sum incorrect");
 	}
 	if (input[n-1]==d) {
 		std::cout<<"no next vector\n";
@@ -90,9 +91,9 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& xs) {
     return os << "}";
 }
 
-/*
+
 std::vector<std::vector<short>> vectors(short n,short d) {
-	std::vector<int> vec(n+1);
+	std::vector<short> vec(n+1);
 	vec[0]=d;
 	int N=binomial(n+d,d);
 	std::vector<std::vector<short>> output;
@@ -104,7 +105,7 @@ std::vector<std::vector<short>> vectors(short n,short d) {
 	}
 	return output;
 }
-*/
+
 
 
 
@@ -134,31 +135,30 @@ std::vector<std::vector<std::vector<short>>> sum_matrix(short n,short d) {
 }
 
 std::vector<std::vector<std::vector<short>>> tuples(short n, short d) {
-	std::vector<short> iterator_vec=sum_matrix(n,d)[0][0];
-	std::vector<short> zero{};
+	std::vector<std::vector<std::vector<short>>> sum=sum_matrix(n,d);
+	std::vector<short> iterator_vec;
 	std::vector<std::vector<short>> current_tuple;
 	std::vector<std::vector<std::vector<short>>> output;
 	int N=binomial(n+d,d);
-	std::vector<std::vector<std::vector<short>>> sum=sum_matrix(n,d);
 	for (short i=0;i<N;++i) {
 		for (short j=i;j<N;++j) {
 			current_tuple={};
-			if (sum[i][j]==zero) {
+			if (sum[i][j].size()==0) {
 				continue;
 			}
 			else {
 				iterator_vec=sum[i][j];
 			}
 			for (short k=i;k<N;++k) {
-				for (short l=i+1;l<N;++l) {
+				for (short l=k;l<N;++l) {
 					// std::cout<<i<<" "<<j<<" "<<k<<" "<<l<<"\n";
 					if (sum[k][l]==iterator_vec && (i!=k || j!=l)) {
 						current_tuple.push_back({k,l});
-						sum[k][l]=zero;
+						sum[k][l]={};
 					}
 				}
 			}
-			sum[i][j]=zero;
+			sum[i][j]={};
 			if (current_tuple.size()!=0) {
 				current_tuple.push_back({i,j});
 				output.push_back(current_tuple);
@@ -168,15 +168,65 @@ std::vector<std::vector<std::vector<short>>> tuples(short n, short d) {
 	return output;
 }
 
+std::string pad(std::string s, int n) {
+	if (n==0) {
+		return s;
+	}
+	for (int i=0;i<n;++i) {
+		s+=" ";
+	}
+	return s;
+}
+
+int matrix_max_length(std::vector<std::vector<int>> in) {
+	int max=0;
+	for (int i=0;i<in.size();++i) {
+		for (int j=0;j<in[0].size();++j) {
+			if (std::to_string(in[i][j]).length()>max) {
+				max=std::to_string(in[i][j]).length();
+			}
+		}
+	}
+	return max;
+}
+
+void print_matrix(std::vector<std::vector<int>> in, char x, char y) {
+	int max_length=matrix_max_length(in);
+	std::cout<<"\n   "<<x<<"=";
+	for (int k=0;k<in[0].size();++k) {
+		std::cout<<pad(std::to_string(k),max_length-(std::to_string(k)).length())<<" ";
+	}
+	std::cout<<"\n";
+	for (int i=0;i<in.size();++i) {
+		for (int j=0;j<in[i].size();++j) {
+			if (i==0 && j==0) {
+				std::cout<<y<<"=0 ";
+			}
+			if (i!=0 && j==0) {
+				std::cout<<" "<<i<<"  ";
+				//std::cout<<" "<<pad(std::to_string(i),max_length-(std::to_string(i)).length());
+			}
+			std::cout<<" "<<pad(std::to_string(in[i][j]),max_length-(std::to_string(in[i][j])).length());
+		}
+		std::cout<<"\n";
+	}
+}
+
+
+
 
 int main() {
 
-short n=5;
-short d=6;
+short n=6;
+short d=1;
+//2*d+n+1 choose n?
 
-//std::cout<<"\ntuples with sums that appear at least twice: \n"<<tuples(n,d)<<"\n";
+auto output=tuples(n,d);
+auto vector_output=vectors(n,d);
 
-std::cout<<"\n"<<(tuples(n,d)).size()<<(tuples(n,d).size()==1 ? " sum appears" : " sums appear")<<" at least twice";
+//std::cout<<"\ntuples with sums that appear at least twice: \n"<<output<<"\n";
+
+std::cout<<"\n"<<(output).size()<<(output.size()==1 ? " sum appears" : " sums appear")<<" at least twice";
 
 
 /*
@@ -187,18 +237,49 @@ for (int D=0;D<6;++D) {
 }
 */
 
+
+unsigned long eq_counter=0;
+for (int r=0;r<output.size();++r) {
+	std::vector<std::vector<short>> tuple_r=output[r];
+	eq_counter+=((tuple_r.size()-1)*(tuple_r.size()))/2;
+}
+std::cout<<"\nthese sums create a total number of "<<eq_counter<<" equations.";
+
+
 /*
 std::cout<<"\ncorresponding to the following additions:" ;
-std::vector<int> iterate_tuple;
-for (int r=0;r<tuples.size();++r) {
-	std::vector<std::vector<int>> tuple_r=tuples[r];
+std::vector<short> iterate_tuple;
+for (int r=0;r<output.size();++r) {
+	std::vector<std::vector<short>> tuple_r=output[r];
 	std::stringstream s;
 	for (short m=0;m<tuple_r.size();++m) {
 		iterate_tuple=tuple_r[m];
-		s<<vectors[iterate_tuple[0]]<<" + "<<vectors[iterate_tuple[1]]<<((m==tuple_r.size()-1) ? "" : " = ");
+		s<<vector_output[iterate_tuple[0]]<<" + "<<vector_output[iterate_tuple[1]]<<((m==tuple_r.size()-1) ? "" : " = ");
 	}
 	std::cout<<"\n"<<s.str();
 }
 */
+
+
+int dim_n=3;
+int dim_d=19;
+unsigned long count=0;
+std::vector<std::vector<short>> it_tup;
+std::vector<std::vector<int>> n_d_matrix(dim_n, std::vector<int>(dim_d,0));
+for (int i=0;i<dim_n;++i) {
+	for (int j=0;j<dim_d;++j) {
+		count=0;
+		auto tup=tuples(i,j);
+		for (int r=0;r<tup.size();++r) {
+			it_tup=tup[r];
+			count+=((it_tup.size()-1)*(it_tup.size()))/2;
+		}
+		n_d_matrix[i][j]=count;
+	}
+}
+print_matrix(n_d_matrix,'d','n');
+
+
+
 
 }
